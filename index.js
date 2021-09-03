36a3bea4334c62769fa021805fdc8ecba7bbb963
Nicholas Loubser <nicholas@Nicholass-MacBook-Air.local>
2021-09-03 10:02:29 +0200

    Works on the devnet
    the goal is now to get this to work on the mainnet

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import * as solanaWeb3 from "@solana/web3.js";
-import * as splToken from "@solana/spl-token";
+const solanaWeb3 = require("@solana/web3.js");
+const splToken = require("@solana/spl-token");
 
 async function createNFT() {
 
@@ -32,11 +32,80 @@ async function createNFT() {
 
     // create new token mint
     let mint = await splToken.Token.createMint(
-        connection,
-        fromWallet,
+        connection, // connection to the solana network
+        fromWallet, // the account that will pay the fee
+        // the public key that has the authority to mint tokens of this type
         fromWallet.publicKey,
+        // the public key that can freeze tokens of this type (optional)
         null,
+        // amount of decimal places for the token. Solana tokens have 9 places
         9,
+        // this creates the account (mint) associated with public key.
+        // The chain of custody is as follows: the NFT resides in the account
+        // and the wallet owns this account (keys -> wallet -> account)
         splToken.TOKEN_PROGRAM_ID
     );
+
+    // get the token account of the fromWallet Solana adress, if it does not
+    // exist, create it
+    let fromTokenAccount = await mint.getOrCreateAssociatedAccountInfo(
+        fromWallet.publicKey
+    );
+
+    // The following code block creates a wallet with a separate set of
+    // public/secret keys and then creates an account linking the mint variable
+    // to the newly created wallet above.
+
+    // generate a new wallet to recieve newly minted token
+    let toWallet = solanaWeb3.Keypair.generate();
+
+    // get the token account of the toWallet Solana address if it does not
+    // exist, create it
+    let toTokenAccount = await mint.getOrCreateAssociatedAccountInfo(
+        toWallet.publicKey
+    );
+
+    // minting 1 new token to the toTokenAccount account just returned/created
+    await mint.mintTo(
+        fromTokenAccount.address, // who it is going to
+        fromWallet.publicKey, // minting authority
+        [], // multisig
+        1000000000 // how many? exactly one token
+    );
+
+    // the most crucial part of the process. SetAuthority() ensures that
+    // additional tokens of this type cannot be created. This action cannot be
+    // undone
+    await mint.setAuthority(
+        mint.publicKey, // account of the token
+        null, // new authority to be set
+        "MintTokens", // type of authority that the account currently has
+        fromWallet.publicKey, // public key of the current authority holder
+        [] // array of signers
+    );
+
+    // add token transfer instructions to transaction
+    let transaction = new solanaWeb3.Transaction().add(
+        splToken.Token.createTransferInstruction(
+            splToken.TOKEN_PROGRAM_ID,
+            fromTokenAccount.address,
+            toTokenAccount.address,
+            fromWallet.publicKey,
+            [],
+            1
+        )
+    );
+
+    // sign transaction, broadcast, and confirm
+    let signature = await solanaWeb3.sendAndConfirmTransaction(
+        connection,
+        transaction,
+        [fromWallet],
+        {commitment: "confirmed"}
+    );
+
+    console.log("SIGNATURE", signature);
 };
+
+createNFT();
+
